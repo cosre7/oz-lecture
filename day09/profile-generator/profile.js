f5e88cec49de93014b2af01d7d2e7a96c1d794de
cosre7 <cosre7@gmail.com>
2025-07-08 16:40:26 +0900

    day09 과제 수정

--- day09/profile-generator/profile.js
+++ day09/profile-generator/profile.js
@@ -10,15 +10,13 @@ let age = 33;
 
 // 변수로 프로필 출력
 console.log(
-    "안녕하세요! 저는 " +
+    "<변수로 프로필 출력> \n안녕하세요! 저는 " +
         name +
         "입니다. \n나이는 " +
         age +
         "살 입니다. \t 학생" +
         isStudent
 );
-
-console.log("------------------------------------------------");
 
 // 취미
 let hobbies = ["youtube", "reading", "tv", "gaming"];
@@ -33,7 +31,7 @@ let profile = {
 
 // 객체로 프로필 출력
 console.log(
-    "이름은 " +
+    "<객체로 프로필 출력>\n이름은 " +
         profile.name +
         "이고, 나이는 " +
         profile.age +
@@ -43,6 +41,7 @@ console.log(
 );
 
 // typeof 출력
+console.log("<typeof 출력>");
 console.log("typeof profile :", typeof profile);
 console.log("typeof hobbies :", typeof hobbies);
 console.log("typeof name :", typeof name);
@@ -52,24 +51,28 @@ console.log("typeof isStudent :", typeof isStudent);
 // null과 undefined
 let nullVar = null;
 let undefinedVar = undefined;
+console.log("<null과 undefined>");
 console.log("nullVar :", typeof nullVar);
 console.log("undefinedVar :", typeof undefinedVar);
-console.log(nullVar === undefinedVar);
+console.log("nullVar === undefinedVar :", nullVar === undefinedVar);
 
 // 백틱으로 프로필 출력
 console.log(
-    `이름은 ${profile.name}이고, 나이는 ${profile.age}살 입니다. 취미는 ${profile.hobbies}입니다.`
+    `<백틱으로 프로필 출력>
+이름은 ${profile.name}이고, 나이는 ${profile.age}살 입니다. 취미는 ${profile.hobbies}입니다.`
 );
 
 // 배열에 null 추가
 hobbies.push(null);
-console.log(typeof hobbies[0]);
-console.log(typeof hobbies[1]);
-console.log(typeof hobbies[2]);
-console.log(typeof hobbies[3]);
-console.log(typeof hobbies[4]);
-console.log(hobbies);
+console.log('<배열에 "null" 추가>');
+console.log("typeof hobbies[0] :", typeof hobbies[0]);
+console.log("typeof hobbies[1] :", typeof hobbies[1]);
+console.log("typeof hobbies[2] :", typeof hobbies[2]);
+console.log("typeof hobbies[3] :", typeof hobbies[3]);
+console.log("typeof hobbies[4] :", typeof hobbies[4]);
+console.log("hobbies :", hobbies);
 
 // 객체에 속성 추가
 profile.email = null;
-console.log(profile.email);
+console.log("<객체에 email = null 추가>");
+console.log("profile.email :", profile.email);
